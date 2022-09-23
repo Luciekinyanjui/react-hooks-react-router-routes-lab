8565380d81ccd59ce61c9cc51ae8f662e5da21c5
Luciekinyanjui <lucy.wambui@moringaschool.com>
2022-09-23 23:59:53 +0300

    Lab Complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,12 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return <div>{/*{code here}*/}</div>;
+  <div className="navbar">
+  <NavLink exact to="/">Home</NavLink>
+  <NavLink exact to="/movies">Movies</NavLink>
+  <NavLink exact to="/directors">Directors</NavLink>
+  <NavLink exact to="/actors">Actors</NavLink>
+</div>);
 }
 
 export default NavBar;
